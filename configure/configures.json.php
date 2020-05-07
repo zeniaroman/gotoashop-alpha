f84973cd3f3bcf4573fd6b35b0783a7ca64c0a43
AaZz Inc <zeniaroman@gmail.com>
2020-05-07 21:35:19 +0300

    Test

--- configure/configures.json.php
+++ configure/configures.json.php
@@ -2,7 +2,7 @@
 	"dsn": {
 		"sqlite": "sqlite:'database.db'",
 		"mysql": {
-			"dsn": "mysql:host=localhost;dbname=projectx;charset=utf8",
+			"dsn": "mysql:host=localhost;dbname=project;charset=utf8",
 			"user": "root",
 			"pass": "root"
 		}
