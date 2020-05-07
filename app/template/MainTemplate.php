2797ac38e714bfda3089080140bd1fabe165f83c
AaZz Inc <zeniaroman@gmail.com>
2020-05-08 00:32:22 +0300

    Test

--- app/template/MainTemplate.php
+++ app/template/MainTemplate.php
@@ -4,6 +4,10 @@ $reg = App\system\factory\Registry::i();
 $clientFiles = $reg->app()->getFrontEndClientFiles();
 
 // var_dump($reg->path()->root());
+var_dump($_SERVER);
+var_dump('====================');
+var_dump('====================');
+var_dump('====================');
 var_dump($request->getPath());
 var_dump($request->getEntity());
 var_dump($request->getAction());
