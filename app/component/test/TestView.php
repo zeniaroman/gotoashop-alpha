b4287e0f42f79aededcedac5abc8a09b4ed44863
AaZz Inc <zeniaroman@gmail.com>
2020-05-08 15:23:18 +0300

    added test

--- app/component/test/TestView.php
+++ app/component/test/TestView.php
@@ -1,1 +1,13 @@
-TEST PAGE
+<?php
+
+$method = 'aes-256-cbc';
+
+$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+$hashId =  openssl_encrypt('Test', $method, '111', 0, $iv);
+
+var_dump($hashId);
+
+
+$decrypt = openssl_decrypt($hashId, $method, '111', 0, $iv);
+
+var_dump($decrypt);
