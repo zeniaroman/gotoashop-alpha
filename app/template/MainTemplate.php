c1540bbab0327d66be5783ceea30dfa963e17dd9
AaZz Inc <zeniaroman@gmail.com>
2020-05-10 18:58:16 +0300

    Update front

--- app/template/MainTemplate.php
+++ app/template/MainTemplate.php
@@ -24,7 +24,7 @@ $clientFiles = $regApp->getFrontEndClientFiles();
     p, h1, h2, h3, h4{cursor:default;}
     .header{z-index: 1000;width: 100%;padding: 10px;background: #fff;box-shadow: 0 0 2px 0 rgba(0, 0, 0, .25);transition: all ease .5s;}
     .header_white{background: #fff;}
-    .header__wrap{display: flex;justify-content: space-between;align-items: center;width: 100%;max-width: 1440px;margin: 0 auto;}
+    .header__wrap{display: flex;flex-wrap: wrap;justify-content: space-between;align-items: center;width: 100%;max-width: 1440px;margin: 0 auto;}
     .header__logo{padding: 10px; color: #000;font-size: 1.25em;text-decoration: none;}
     .header__nav{display: flex;justify-content: space-between;align-items: center;margin-right: 10px;}
     .header__link{margin-left: 10px;padding: 10px;color: #2E353E;text-decoration: none;}
